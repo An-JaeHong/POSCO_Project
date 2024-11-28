f2586eae2c1e39d28af2f2b9f9ee2c61e8915dd7
jaehong <jaehongan56@gmail.com>
2024-11-28 11:38:15 +0900

    Inventory 구현 중 Player에게 slectMonster List 전달

--- Assets/POSCO/Scripts/InventoryUI.cs
+++ Assets/POSCO/Scripts/InventoryUI.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
 
-    public GameObject menu;
-    public GameObject monsterCard;
-    public GameObject showSellectMonster;
+    public GameObject inventory;
+    public GameObject monsterCardBackground;
+    public GameObject showSelectMonsterBackground;
 
     public Button showMonster;
     public Button[] monsterCardNum;
@@ -17,80 +18,129 @@ public class InventoryUI : MonoBehaviour
     public Button Reset;
 
     public List<GameObject> playerMonsters = new List<GameObject>();
-    public List<GameObject> tempMonsters = new List<GameObject>();
+    public List<GameObject> tempSelectedMonsters = new List<GameObject>();
 
+    private bool isOpenInventory = false;
     private int choiceNum = 0; //최대3마리 몬스터 
 
     private Player player;
 
+    
+
+    private void init()
+    {
+        choiceNum = 0;
+
+        foreach ( var button in monsterCardNum)
+        {
+           button.interactable = false;
+        }
+
+        tempSelectedMonsters.Clear();
+    }
+
     private void Awake()
     {
-        player = FindObjectOfType<Player>();
+        //player = FindObjectOfType<Player>();
     }
     private void Start()
     {
-        menu.SetActive(false);
-        monsterCard.SetActive(false);
-        showSellectMonster.SetActive(false);
+        player = FindObjectOfType<Player>();
+        init();
+        inventory.SetActive(false);
+        monsterCardBackground.SetActive(false);
+        showSelectMonsterBackground.SetActive(false);
         playerMonsters = player.playerMonsters;
+    }
+
+    private void OnEnable()
+    {
+        init();
     }
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.I))
         {
-            OpenMemu();
+            if (isOpenInventory == false)
+            {
+                OpenInventory();
+                isOpenInventory = true;
+            }
+            else
+            {
+                inventory.SetActive(false);
+                monsterCardBackground.SetActive(false);
+                showSelectMonsterBackground.SetActive(false);
+                isOpenInventory = false;
+            }
+            
         }
 
     }
 
-    private void OpenMemu()
+    private void OpenInventory()
     {
-        menu.SetActive(true);
+        init();// 수정필요
+
+        //inventory 활성화하면 ShowMonsterButton 활성화
+        inventory.SetActive(true);
+        
     }
-
-
     public void OnShowMonsterButton()
     {
-        monsterCard.SetActive(true);
+        //monsterCardBackground 활성화하면 cardButton 활성화
+        monsterCardBackground.SetActive(true);
+        choiceBattleMonster.interactable = true;
+    }
+
+    public void OnChoiceBattleMonsterButton()
+    {
+        foreach (var button in monsterCardNum)
+        {
+            button.interactable = true;
+        }
+        showSelectMonsterBackground.SetActive(true);
+        choiceBattleMonster.interactable = false;
     }
 
     public void OnCardButton(int number)
     {
-       
-        print(number);
+      
         if (choiceNum < 3)
         {
             switch (number)
             {
                 case 0:
-                    tempMonsters.Add(playerMonsters[0]);
+                    tempSelectedMonsters.Add(playerMonsters[0]);
                     monsterCardNum[0].interactable = false;
                     print("첫번째 카드 비활성화");
+                    print($"{playerMonsters[0]} 임시저장됨");
 
                     break;
                 case 1:
-                    tempMonsters.Add(playerMonsters[1]);
+                    tempSelectedMonsters.Add(playerMonsters[1]);
                     monsterCardNum[1].interactable = false;
                     print("두번째 카드 비활성화");
+                    print($"{playerMonsters[1]} 임시저장됨");
                     break;
                 case 2:
-                    tempMonsters.Add(playerMonsters[2]);
-
+                    tempSelectedMonsters.Add(playerMonsters[2]);
                     monsterCardNum[2].interactable = false;
+                    print($"{playerMonsters[3]} 임시저장됨");
                     print("세번째 카드 비활성화");
                     break;
                 case 3:
-                    tempMonsters.Add(playerMonsters[3]);
-
+                    tempSelectedMonsters.Add(playerMonsters[3]);
                     monsterCardNum[3].interactable = false;
                     print("네번째카드 비활성화");
+                    print($"{playerMonsters[4]} 임시저장됨");
                     break;
                 case 4:
-                    tempMonsters.Add(playerMonsters[4]);
-
+                    tempSelectedMonsters.Add(playerMonsters[4]);
                     monsterCardNum[4].interactable = false;
                     print("다섯번째카드 비활성화");
+                    print($"{playerMonsters[4]} 임시저장됨");
                     break;
             }
             choiceNum++;
@@ -101,10 +151,46 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
+    public void OnSellectBoutton()
+    {
+        print("Sellect 누름");
+
+        if (choiceNum == 3)
+        {
 
 
+            print("선택됨");
+
+            //플레이어 몬스터에 정보 넣어야함
+            player.SetSelectedMonsters(tempSelectedMonsters);
 
 
+         
+        }
+
+
+        //삭제예정 stack으로 구현할 예정입니다.
+        //지금은 선택누를시 모두 종료됨
+        
+        inventory.SetActive(false);
+        monsterCardBackground.SetActive(false);
+        showSelectMonsterBackground.SetActive(false);
+        
+    }
+
+    public void OnRestetButton()
+    {
+        print("초기화됨");
+        choiceNum = 0;
+
+        foreach (var button in monsterCardNum)
+        {
+            button.interactable = true;
+        }
+
+        tempSelectedMonsters.Clear();
+    }
 
 
 }
+    
--- Assets/POSCO/Scripts/Player.cs
+++ Assets/POSCO/Scripts/Player.cs
@@ -7,10 +7,40 @@ public class Player : MonoBehaviour
     //플레이어가 시작시 가지고 있는 몬스터
     public List<GameObject> playerMonsters = new List<GameObject>();
     //플레이어가 전투를 위해 선택한 몬스터
-    public List<GameObject> sellectedMonsters = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> selectedMonsters = new List<GameObject>();
 
-    public void SetSellectedMonsters()
+
+
+    public void SetSelectedMonsters(List<GameObject> selecedMonater)
     {
-    
+        List<GameObject> temp = new List<GameObject>();
+        temp = selecedMonater;
+        this.selectedMonsters = temp;
+        print(selectedMonsters[1].name);
     }
+
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            //nt($"{selectedMonsters[0].name}");
+            if(selectedMonsters.Count == 0)
+            {
+                print("전투몬스터를 설정하세요");
+            }
+            else
+            {
+                
+                    print("1");
+
+                
+            }
+
+        }
+    }
+
 }
