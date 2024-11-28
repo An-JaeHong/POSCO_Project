83ce336cd2acdfb8c65a26c6a9ec86f188dffc81
SIRO\gmlwo <gmlwo990518@naver.com>
2024-11-28 13:40:03 +0900

    UIpopup, PlayerStateBase, PlayerIdleState, PlayerContactEnemyState, PlayerBattleState 스크립트 생성

---
+++ Assets/POSCO/Scripts/PlayerStateBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//플레이어 상태를 나타내는 클래스 -> 추상함수로 선언해서 관리할 것임.
+public abstract class PlayerStateBase
+{
+    protected Player player;
+    //아래 두개는 필요 없을 수도 있다.
+    protected GameManager gameManager;
+    protected UIPopup uiPopup;
+
+    public 
+}
