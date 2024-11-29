776df3af4e24c4846915fa12077926f97cbd5ff9
SIRO\gmlwo <gmlwo990518@naver.com>
2024-11-29 13:23:01 +0900

    Element 위치 수정

--- Assets/POSCO/Scripts/Monster.cs
+++ Assets/POSCO/Scripts/Monster.cs
@@ -1,7 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public enum Element
+{
+    None,
+    Fire,
+    Water,
+    Grass,
+}
 
 public class Monster : MonoBehaviour
 {
--- Assets/POSCO/Scripts/TextureMonster.cs
+++ Assets/POSCO/Scripts/TextureMonster.cs
@@ -3,16 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-[Serializable]
-public enum Element
-{
-    FIRE,
-    WATER,
-    GRASS
-}
 public class UIMonster : MonoBehaviour
 {
     public Element element;
